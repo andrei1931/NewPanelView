b91c81237fcf56c16c4e915e5d3c6fb6ba472a1d
andrei1931 <andreipasca904@gmail.com>
2024-04-28 19:35:36 +0300

    design

--- app/src/main/java/com/example/pv_menu/ApiClient.kt
+++ app/src/main/java/com/example/pv_menu/ApiClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
-    private const val BASE_URL = "http://10.111.10.194:8080/"
+    private const val BASE_URL = "http://192.168.1.34:8080/"
 
 
 
